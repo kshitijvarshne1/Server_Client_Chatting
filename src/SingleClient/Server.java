626e5e0a5eb57baf0c3524d6a8983811284ce249
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-18 20:06:27 +0530

    create Server.java

---
+++ src/SingleClient/Server.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 18-Apr-21
+ *   Time: 8:06 PM
+ *   File: Server.java
+ */
+
+package SingleClient;
+
+public class Server {
+}
+
