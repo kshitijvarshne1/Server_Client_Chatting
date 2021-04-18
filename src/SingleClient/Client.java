668f29d9b5fcf9c0a83eee5e053dd9cee3d3c970
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-18 20:29:41 +0530

    add code for send and receive message to server

--- src/SingleClient/Client.java
+++ src/SingleClient/Client.java
@@ -7,13 +7,22 @@
 
 package SingleClient;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) throws IOException {
         Socket socket = new Socket("localhost",4000);
         System.out.println("Connected to server");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+
+        Scanner sc = new Scanner(System.in);
     }
 }
 
