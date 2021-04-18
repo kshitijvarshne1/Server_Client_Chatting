797f7ee92d779f5d9ed710d2b3fa205b45d07af1
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-18 20:11:35 +0530

    add code for client connection

--- src/SingleClient/Client.java
+++ src/SingleClient/Client.java
@@ -7,6 +7,13 @@
 
 package SingleClient;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class Client {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost",4000);
+        System.out.println("Connected to server");
+    }
 }
 
