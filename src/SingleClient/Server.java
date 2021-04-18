aee00291ab31c0a2d8e05e37a3d0058de0ee9ee2
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-18 20:34:27 +0530

    add code for send acknowledge to client

--- src/SingleClient/Server.java
+++ src/SingleClient/Server.java
@@ -7,7 +7,10 @@
 
 package SingleClient;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -18,6 +21,17 @@ public class Server {
         System.out.println("Waiting for client connection");
         Socket clientSocket = socket.accept();
         System.out.println("Client connected");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),true);
+        String msg;
+
+        do{
+            msg=in.readLine();
+            System.out.println("Client send :- "+msg);
+            out.println("Server acknowledge this message :- "+msg);
+        }while(!msg.equals("exist"));
+
     }
 }
 
