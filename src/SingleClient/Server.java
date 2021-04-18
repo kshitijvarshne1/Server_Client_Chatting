751ee2603e53e9dd974288ec39f90b8eb480aace
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-18 20:10:09 +0530

    Initialization the server for connection

--- src/SingleClient/Server.java
+++ src/SingleClient/Server.java
@@ -7,9 +7,17 @@
 
 package SingleClient;
 
-public class Server {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket socket = new ServerSocket(4000);
+        System.out.println("Waiting for client connection");
+        Socket clientSocket = socket.accept();
+        System.out.println("Client connected");
     }
 }
 
