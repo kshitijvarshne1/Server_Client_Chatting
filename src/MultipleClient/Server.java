7dd2d68786d1617cbfa4e1b8da5628254672ac9c
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-19 15:48:54 +0530

    add ServerSocket and client socket

--- src/MultipleClient/Server.java
+++ src/MultipleClient/Server.java
@@ -7,6 +7,23 @@
 
 package MultipleClient;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    public static void main(String[] args) throws IOException {
+        ServerSocket socket = new ServerSocket(4000);
+        System.out.println("Waiting for connection");
+        Socket client = socket.accept();
+        System.out.println("Client connected");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        PrintWriter out = new PrintWriter(client.getOutputStream(),true);
+
+    }
 }
 
