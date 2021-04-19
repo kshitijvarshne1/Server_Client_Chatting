f56228120fbe7bed35c69e6e5da231ae4cd94303
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-19 15:46:05 +0530

    create Server.java

---
+++ src/MultipleClient/Server.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 19-Apr-21
+ *   Time: 3:45 PM
+ *   File: Server.java
+ */
+
+package MultipleClient;
+
+public class Server {
+}
+
