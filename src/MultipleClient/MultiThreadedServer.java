31c719b140c170010f4e4d496dc34c354450a0f6
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-19 15:59:35 +0530

    create MultiThreadedServer.java

---
+++ src/MultipleClient/MultiThreadedServer.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 19-Apr-21
+ *   Time: 3:59 PM
+ *   File: MultiThreadedServer.java
+ */
+
+package MultipleClient;
+
+public class MultiThreadedServer {
+}
+
