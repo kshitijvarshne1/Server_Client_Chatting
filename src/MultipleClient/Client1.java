788310bd7eea9a017664a9f37fbf50341ad5d4ba
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-19 15:56:23 +0530

    add code to input msg from user and send to user

--- src/MultipleClient/Client1.java
+++ src/MultipleClient/Client1.java
@@ -7,6 +7,29 @@
 
 package MultipleClient;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client1 {
+    public static void main(String[] args) throws IOException {
+        Socket socket= new Socket("localhost",4000);
+        System.out.println("Connected to the server");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+
+        Scanner sc = new Scanner(System.in);
+        String msg;
+        do{
+            msg=sc.nextLine();
+            out.println(msg);
+            String response = in.readLine();
+            System.out.println(response);
+        }while(!msg.equals("exist"));
+    }
 }
 
