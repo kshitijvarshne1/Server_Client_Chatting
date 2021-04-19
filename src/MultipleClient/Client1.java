9bcd6cda6f865a7c62ea9868c19e6ba00ec473d9
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-19 15:52:21 +0530

    create Client1.java

---
+++ src/MultipleClient/Client1.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 19-Apr-21
+ *   Time: 3:52 PM
+ *   File: Client1.java
+ */
+
+package MultipleClient;
+
+public class Client1 {
+}
+
